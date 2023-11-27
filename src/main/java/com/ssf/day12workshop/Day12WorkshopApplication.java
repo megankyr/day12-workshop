e6c29b34f9317b86864d1da682c11a0a50cccb1c
Megan <megan.koh96@gmail.com>
2023-11-27 10:12:59 +0800

    Initial Commit

--- src/main/java/com/ssf/day12workshop/Day12WorkshopApplication.java
+++ src/main/java/com/ssf/day12workshop/Day12WorkshopApplication.java
@@ -1,5 +1,9 @@
 package com.ssf.day12workshop;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day12WorkshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day12WorkshopApplication.class, args);
+		SpringApplication app = new SpringApplication(Day12WorkshopApplication.class);
+		String port = portFromCommandLine(args);
+		if (port == null) {
+			port = "3000";
+		}
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		System.out.println("Application started on " + port);
+		app.run(args);
 	}
 
+	private static String portFromCommandLine(String[] args) {
+		ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+		if (cliOpts.containsOption("port")) {
+			return cliOpts.getOptionValues("port").get(0);
+		}
+		if (cliOpts.containsOption("server.port")) {
+			return cliOpts.getOptionValues("server.port").get(0);
+		}
+		return null;
+	}
 }
